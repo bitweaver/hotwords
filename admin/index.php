03cded9ff515f6d716f43cdf6f586d87b978db35
Max Kremmel <xing@synapse.plus.com>
2005-08-01 18:40:29 +0000

    merge recent changes with HEAD - R1 and HEAD are identical now

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_hotwords/admin/index.php,v 1.1 2005/06/19 04:50:43 bitweaver Exp $
+// $Header: /cvsroot/bitweaver/_bit_hotwords/admin/index.php,v 1.2 2005/08/01 18:40:29 squareing Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -12,14 +12,14 @@ require_once( '../../bit_setup_inc.php' );
 include_once ( HOTWORDS_PKG_PATH.'hotword_lib.php' );
 
 if ($feature_hotwords != 'y') {
-	$smarty->assign('msg', tra("This feature is disabled").": feature_hotwords");
+	$gBitSmarty->assign('msg', tra("This feature is disabled").": feature_hotwords");
 
 	$gBitSystem->display( 'error.tpl' );
 	die;
 }
 
 if (!$gBitUser->isAdmin()) {
-	$smarty->assign('msg', tra("You dont have permission to use this feature"));
+	$gBitSmarty->assign('msg', tra("You dont have permission to use this feature"));
 
 	$gBitSystem->display( 'error.tpl' );
 	die;
@@ -29,7 +29,7 @@ if (!$gBitUser->isAdmin()) {
 if (isset($_REQUEST["add"])) {
 	
 	if(empty($_REQUEST["word"]) || empty($_REQUEST["url"])) {
-	        $smarty->assign('msg', tra("You have to provide a hotword and a URL"));
+	        $gBitSmarty->assign('msg', tra("You have to provide a hotword and a URL"));
 		$gBitSystem->display( 'error.tpl' );
 		die;
 	}
@@ -47,7 +47,7 @@ if ( empty( $_REQUEST["sort_mode"] ) ) {
 	$sort_mode = $_REQUEST["sort_mode"];
 }
 
-$smarty->assign_by_ref('sort_mode', $sort_mode);
+$gBitSmarty->assign_by_ref('sort_mode', $sort_mode);
 
 // If offset is set use it if not then use offset =0
 // use the maxRecords php variable to set the limit
@@ -61,7 +61,7 @@ if (isset($_REQUEST['page'])) {
 	$page = &$_REQUEST['page'];
 	$offset = ($page - 1) * $maxRecords;
 }
-$smarty->assign_by_ref('offset', $offset);
+$gBitSmarty->assign_by_ref('offset', $offset);
 
 if (isset($_REQUEST["find"])) {
 	$find = $_REQUEST["find"];
@@ -69,28 +69,28 @@ if (isset($_REQUEST["find"])) {
 	$find = '';
 }
 
-$smarty->assign('find', $find);
+$gBitSmarty->assign('find', $find);
 
 $words = $hotwordlib->list_hotwords($offset, $maxRecords, $sort_mode, $find);
 $cant_pages = ceil($words["cant"] / $maxRecords);
-$smarty->assign_by_ref('cant_pages', $cant_pages);
-$smarty->assign('actual_page', 1 + ($offset / $maxRecords));
+$gBitSmarty->assign_by_ref('cant_pages', $cant_pages);
+$gBitSmarty->assign('actual_page', 1 + ($offset / $maxRecords));
 
 if ($words["cant"] > ($offset + $maxRecords)) {
-	$smarty->assign('next_offset', $offset + $maxRecords);
+	$gBitSmarty->assign('next_offset', $offset + $maxRecords);
 } else {
-	$smarty->assign('next_offset', -1);
+	$gBitSmarty->assign('next_offset', -1);
 }
 
 // If offset is > 0 then prev_offset
 if ($offset > 0) {
-	$smarty->assign('prev_offset', $offset - $maxRecords);
+	$gBitSmarty->assign('prev_offset', $offset - $maxRecords);
 } else {
-	$smarty->assign('prev_offset', -1);
+	$gBitSmarty->assign('prev_offset', -1);
 }
 
 // Get users (list of users)
-$smarty->assign_by_ref('words', $words["data"]);
+$gBitSmarty->assign_by_ref('words', $words["data"]);
 
 
 
