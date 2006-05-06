5f540da885812c4eccaab875bb025cf0ff5c9f56
Max Kremmel <xing@synapse.plus.com>
2006-05-06 15:48:08 +0000

    clean up schema files, remove unused PackageInfo data - we really need a package manager for this, add plugin_path column to liberty_plugins

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -18,15 +18,11 @@ foreach( array_keys( $tables ) AS $tableName ) {
 $gBitInstaller->registerPackageInfo( HOTWORDS_PKG_NAME, array(
 	'description' => "Hotwords allow you to specify particular words that can be associated with a particular link. e.g. if someone writes 'google' in any text, you can associate it with 'http://www.google.com'.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'version' => '0.1',
-	'state' => 'alpha',
-	'dependencies' => '',
 ) );
 
 // ### Default Preferences
 $gBitInstaller->registerPreferences( HOTWORDS_PKG_NAME, array(
 	array(HOTWORDS_PKG_NAME, 'hotwords_new_window','n'),
-	//array(HOTWORDS_PKG_NAME, 'hotwords','y')
 ) );
 
 ?>
